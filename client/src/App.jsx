8124fdec6ae4b168a4f7fe661bc6bcc37b4fff80
Ahmed Abdirizaak <mernstack.js@gmail.com>
2025-12-18 20:16:24 +0100

    Add catelog path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import { Route, Routes } from "react-router";
 import Index from "./pages";
 import NotFound from "./pages/notFound";
+import Profile from "./pages/profile";
+import Catelog from "./pages/catelog";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/catalog" element={<Catelog />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
