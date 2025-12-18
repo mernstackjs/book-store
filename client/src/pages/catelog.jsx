f57ffcdb526d6e79561255c0ab739d89da61778a
Ahmed Abdirizaak <mernstack.js@gmail.com>
2025-12-18 20:16:33 +0100

    Add Catelog page

---
+++ client/src/pages/catelog.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Catelog() {
+  return (
+    <div>
+      <h1>Book Catalog</h1>
+      <p>Browse our collection of [] books</p>
+    </div>
+  );
+}
