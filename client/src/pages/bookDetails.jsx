4dbbce8a8663abca0bc865ac7e7504443afcef52
Ahmed Abdirizaak <mernstack.js@gmail.com>
2025-12-19 18:37:00 +0100

    add and improve book details page

---
+++ client/src/pages/bookDetails.jsx
@@ -0,0 +1,81 @@
+import { ArrowLeft, BookOpen, Calendar, Hash, Star } from "lucide-react";
+
+import { Link, useParams } from "react-router";
+import { books } from "../components/index/featureOf4books";
+
+export default function BookDetails() {
+  const { bookId } = useParams();
+  console.log(bookId);
+  const book = books.find((book) => book.id == bookId);
+  console.log(book);
+  return (
+    <div className="bg-yellow-50 min-h-screen flex flex-col justify-center px-18">
+      <Link className="flex mb-4  items-center gap-2" to="/catalog">
+        <ArrowLeft size={18} /> Back to catelog
+      </Link>
+
+      <div className="flex gap-3">
+        <img className="border h-150" src={book?.image} alt={book?.title} />
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-5 mb-3">
+            <span className="border rounded-2xl  border-gray-200 px-4 py-1 font-medium">
+              {book.category}
+            </span>
+            <span className="border rounded-2xl  bg-green-900/85 text-white px-4 py-1 font-medium">
+              {book.available} available
+            </span>
+          </div>
+          <h1 className="text-2xl font-medium">{book.title}</h1>
+          <p className="text-gray-500 mb-2">{book.author}</p>
+          <span className="flex gap-1 my-3">
+            <Star color="#d29641" /> {book.rating}{" "}
+            <span className="text-gray-400 text-sm">/ 5</span>
+          </span>
+          <div className="border flex gap-2 justify-around items-center my-5 border-gray-200 bg-white rounded-xl w-full p-6">
+            <div className="flex gap-2 items-center justify-center">
+              <Calendar
+                size={40}
+                className="bg-yellow-100 rounded-2xl text-yellow-600 p-2"
+              />
+              <div className="flex flex-col gap-1">
+                <span>Published</span> <span>1960</span>
+              </div>
+            </div>
+            <div className="flex gap-2 items-center justify-center">
+              <Hash
+                size={40}
+                className="bg-yellow-100 rounded-2xl text-yellow-600 p-2"
+              />
+              <div className="flex flex-col gap-1">
+                <span>ISBN</span> <span>978-0061120084</span>
+              </div>
+            </div>
+            <div className="flex gap-3 items-center justify-center">
+              <BookOpen
+                size={40}
+                className="bg-yellow-100 rounded-2xl text-yellow-600 p-2"
+              />
+              <div className="flex flex-col gap-1">
+                <span> Total Copies</span> <span>4</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h3 className="text-xl font-semibold">Description</h3>
+              <p className="text-sm italic">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Aliquam modi dolorum voluptatem, repellendus optio quod
+                reiciendis rem odio ratione officiis. Lorem ipsum dolor sit
+                amet.
+              </p>
+            </div>
+            <button className="bg-amber-500 cursor-pointer mt-8 inline-flex items-center gap-2 rounded-md px-6 py-3 font-bold text-white hover:bg-amber-600 transition">
+              <BookOpen /> Loan This Book
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
