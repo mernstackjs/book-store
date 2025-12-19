2f57da31da6eebbc6a8cbc8aef985ac5a11ee8ef
Ahmed Abdirizaak <mernstack.js@gmail.com>
2025-12-19 16:47:55 +0100

    Add index Hero and display info

---
+++ client/src/components/index/featureOf4books.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FeatureOf4books() {
+  return <div>FeatureOf4books</div>;
+}
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,3 +1,36 @@
+import { BookOpen, Clock8, Users } from "lucide-react";
+import Hero from "../components/index/Hero";
+
 export default function Index() {
-  return <div>Index</div>;
+  return (
+    <div>
+      <Hero />
+      <div className="flex gap-2 justify-around items-center my-2 py-8">
+        <div className="flex flex-col gap-2 items-center">
+          <BookOpen
+            size={40}
+            className="bg-yellow-100 rounded-2xl text-yellow-600 p-2"
+          />
+          <span className="text-2xl font-bold">8</span>
+          <p>Books Available</p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <Users
+            size={40}
+            className="bg-yellow-100 rounded-2xl text-yellow-600 p-2"
+          />
+          <span className="text-2xl font-bold">2,500+</span>
+          <p>Active Members</p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <Clock8
+            size={40}
+            className="bg-yellow-100 rounded-2xl text-yellow-600 p-2"
+          />
+          <span className="text-2xl font-bold">30 Days</span>
+          <p>Loan Period</p>
+        </div>
+      </div>
+    </div>
+  );
 }
