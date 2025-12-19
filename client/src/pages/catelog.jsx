71c35c57b0c758de46bb5c5274870fdf6a84a7d5
Ahmed Abdirizaak <mernstack.js@gmail.com>
2025-12-19 19:07:17 +0100

    Add test api call in frontend

--- client/src/pages/catelog.jsx
+++ client/src/pages/catelog.jsx
@@ -1,10 +1,31 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
 
 export default function Catelog() {
+  const [message, setMessage] = useState(() => {
+    const respMessage = localStorage.getItem("message");
+    return respMessage ? JSON.parse(respMessage) : "";
+  });
+  const testApiCall = async () => {
+    const response = await axios.get("http://localhost:5059/api/health");
+    localStorage.setItem("message", JSON.stringify(response.data.message));
+  };
   return (
     <div>
       <h1>Book Catalog</h1>
       <p>Browse our collection of [] books</p>
+
+      <button
+        onClick={testApiCall}
+        className="text-white bg-amber-900 p-3 rounded-md my-5"
+      >
+        Test Api Call
+      </button>
+
+      <p className="px-12  text-2xl font-extrabold italic">
+        {message ? message : "No message"}
+      </p>
     </div>
   );
 }
