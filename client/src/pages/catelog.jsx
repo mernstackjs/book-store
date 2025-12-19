0ab6b9ceec9e7850ff9d9856bdfbbd97afd72c48
Ahmed Abdirizaak <mernstack.js@gmail.com>
2025-12-19 20:21:39 +0100

    add url

--- client/src/pages/catelog.jsx
+++ client/src/pages/catelog.jsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 
 export default function Catelog() {
   const [message, setMessage] = useState("");
+  const [people, setPeople] = useState([]);
+  const [name, setName] = useState("");
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetTestApi = async () => {
@@ -17,8 +19,33 @@ export default function Catelog() {
 
     fetTestApi();
   }, []);
+  useEffect(() => {
+    const getPeople = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/get-peoples`
+      );
+      setPeople(response.data.peoples);
+      setLoading(false);
+    };
 
-  console.log();
+    getPeople();
+  }, []);
+
+  const addPersonHandle = async () => {
+    try {
+      await axios.post(
+        `${import.meta.env.VITE_API_URL}/add-person`,
+
+        {
+          name,
+        }
+      );
+      setName("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
@@ -34,6 +61,38 @@ export default function Catelog() {
           Loading ......
         </p>
       )}
+
+      <div className="p-4 md:flex gap-2">
+        <div>
+          <h1>Add Person</h1>
+          <input
+            className="border my-3 rounded-md w-full p-3"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Enter Name"
+          />
+          <button
+            className="p-3 mb-3 rounded-md bg-blue-900 text-white"
+            onClick={addPersonHandle}
+          >
+            Add Name
+          </button>
+        </div>
+        <div>
+          <h1>View Peoples</h1>
+          {!loading &&
+            people?.map((person) => (
+              <div
+                key={person.id}
+                className="flex gap-2 bg-amber-300 text-amber-700 p-2 rounded-xl my-2"
+              >
+                <p>{person.id}</p>
+                <p>{person.name}</p>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
