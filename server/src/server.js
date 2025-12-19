56c651a2b16f81ba6b6707c791a2d42a8e5f90d5
Ahmed Abdirizaak <mernstack.js@gmail.com>
2025-12-19 20:19:29 +0100

    add json for express body

--- server/src/server.js
+++ server/src/server.js
@@ -8,9 +8,29 @@ app.use(
   })
 );
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/api/health", (req, res) => {
   res.status(200).json({
     message: "Your server health is OKi",
+  });
+});
+
+const peoples = [];
+app.post("/add-person", (req, res) => {
+  const person = {
+    id: Math.random().toString(36).substring(2, 8),
+    name: req.body.name,
+  };
+  peoples.push(person);
+  res.status(201).json({
+    message: "Add person",
+  });
+});
+app.get("/get-peoples", (req, res) => {
+  res.status(200).json({
+    peoples,
   });
 });
 
